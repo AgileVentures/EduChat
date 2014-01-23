8395d96b96e92b8705f282370dcd931cf234462b
saharon <saharoncld@verizon.net>
2014-01-23 10:57:13 -0500

    fixed the chat  in a popup window js for remote use

--- app/chatframe_popupWin.js
+++ app/chatframe_popupWin.js
@@ -4,7 +4,7 @@
 		return this.each(function(){
 		
 		$(this).click(function(){
-					window.open('popupContent.html','CHAT', "width=600, height=750").focus();
+					window.open('https://rawgithub.com/saharon/EduChat/develop/popupContent.html','CHAT', "width=600, height=750").focus();
 					return false;
 		
 			});		
