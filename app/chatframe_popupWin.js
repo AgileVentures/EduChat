3407ddbad1aad09c218c92b17d31fd8527d28b90
Bryan Yap <bcollie115@gmail.com>
2014-01-26 22:40:07 +0000

    modified url for popupWin.js

--- app/chatframe_popupWin.js
+++ app/chatframe_popupWin.js
@@ -4,7 +4,7 @@
 		return this.each(function(){
 		
 		$(this).click(function(){
-					window.open('https://rawgithub.com/saharon/EduChat/develop/popupContent.html','CHAT', "width=600, height=750").focus();
+					window.open('https://rawgithub.com/yggie/EduChat/develop/popupContent.html','CHAT', "width=600, height=750").focus();
 					return false;
 		
 			});		
