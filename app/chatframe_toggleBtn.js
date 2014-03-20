388b3a31cff884d0bba98c5f7e01baf126b692c0
Bryan Yap <yggie@users.noreply.github.com>
2014-03-20 21:31:08 +0000

    Update chatframe_toggleBtn.js

--- app/chatframe_toggleBtn.js
+++ app/chatframe_toggleBtn.js
@@ -1,4 +1,117 @@
 (function() {
+
+var tmp = {};
+(function (factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['jquery'], factory);
+	} else if (typeof exports === 'object') {
+		// CommonJS
+		factory(require('jquery'));
+	} else {
+		// Browser globals
+		factory(jQuery);
+	}
+}(function ($) {
+
+	var pluses = /\+/g;
+
+	function encode(s) {
+		return config.raw ? s : encodeURIComponent(s);
+	}
+
+	function decode(s) {
+		return config.raw ? s : decodeURIComponent(s);
+	}
+
+	function stringifyCookieValue(value) {
+		return encode(config.json ? JSON.stringify(value) : String(value));
+	}
+
+	function parseCookieValue(s) {
+		if (s.indexOf('"') === 0) {
+			// This is a quoted cookie as according to RFC2068, unescape...
+			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+		}
+
+		try {
+			// Replace server-side written pluses with spaces.
+			// If we can't decode the cookie, ignore it, it's unusable.
+			// If we can't parse the cookie, ignore it, it's unusable.
+			s = decodeURIComponent(s.replace(pluses, ' '));
+			return config.json ? JSON.parse(s) : s;
+		} catch(e) {}
+	}
+
+	function read(s, converter) {
+		var value = config.raw ? s : parseCookieValue(s);
+		return $.isFunction(converter) ? converter(value) : value;
+	}
+
+	var config = tmp.cookie = function (key, value, options) {
+
+		// Write
+
+		if (value !== undefined && !$.isFunction(value)) {
+			options = $.extend({}, config.defaults, options);
+
+			if (typeof options.expires === 'number') {
+				var days = options.expires, t = options.expires = new Date();
+				t.setTime(+t + days * 864e+5);
+			}
+
+			return (document.cookie = [
+				encode(key), '=', stringifyCookieValue(value),
+				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+				options.path    ? '; path=' + options.path : '',
+				options.domain  ? '; domain=' + options.domain : '',
+				options.secure  ? '; secure' : ''
+			].join(''));
+		}
+
+		// Read
+
+		var result = key ? undefined : {};
+
+		// To prevent the for loop in the first place assign an empty array
+		// in case there are no cookies at all. Also prevents odd result when
+		// calling $.cookie().
+		var cookies = document.cookie ? document.cookie.split('; ') : [];
+
+		for (var i = 0, l = cookies.length; i < l; i++) {
+			var parts = cookies[i].split('=');
+			var name = decode(parts.shift());
+			var cookie = parts.join('=');
+
+			if (key && key === name) {
+				// If second argument (value) is a function it's a converter...
+				result = read(cookie, value);
+				break;
+			}
+
+			// Prevent storing a cookie that we couldn't decode.
+			if (!key && (cookie = read(cookie)) !== undefined) {
+				result[name] = cookie;
+			}
+		}
+
+		return result;
+	};
+
+	config.defaults = {};
+
+	tmp.removeCookie = function (key, options) {
+		if ($.cookie(key) === undefined) {
+			return false;
+		}
+
+		// Must not alter options, thus extending a fresh object...
+		$.cookie(key, '', $.extend({}, options, { expires: -1 }));
+		return !$.cookie(key);
+	};
+
+}));
+
 var pathname = window.location.pathname;
 var chatChannels = "#"+pathname.split('/')[3];
 //var chatChannels = $("#chatframe").attr('chatroom');//"#cs1691x";
@@ -21,7 +134,7 @@ $("#chatiframe").on('load', function() {
 	showDelayed(5, this, 'loadingchat');
 });
 
-if ($.cookie('chat_default') === "closed" || window.outerWidth < 750) {
+if (tmp.cookie('chat_default') === "closed" || window.outerWidth < 750) {
 	window.onload = function() {
 		$("#chat-button").one("click", function() {
 		    init();
@@ -122,14 +235,14 @@ function showDelayed(delaySeconds, element, elementHide) {
     //document.getElementById(elementHide).style.display='none';
                 $("#chatframe").removeClass('minimized');
                 $("#chat-button").html("Hide Chat");
-                $.cookie('chat_default', 'open', { expires: 7 });
+                tmp.cookie('chat_default', 'open', { expires: 7 });
                 $('#chat-button').on("click", function () {
                     $('#chatframe').toggleClass("minimized");
                     if ($('#chatframe').hasClass("minimized")) {
-                        $.cookie('chat_default', 'closed', { expires: 7 });
+                        tmp.cookie('chat_default', 'closed', { expires: 7 });
                         $("#chat-button").html("Open Chat");
                     } else {
-                        $.cookie('chat_default', 'open', { expires: 7 });
+                        tmp.cookie('chat_default', 'open', { expires: 7 });
                         $("#chat-button").html("Hide Chat");
                     }
                 });
